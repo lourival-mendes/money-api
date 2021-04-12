e3fc12d6bdc70761915e7b212cdd6553186d24ac
Lourival Mendes <minhavirtude@gmail.com>
2021-04-12 16:17:15 -0300

    pom.xml -> Inserida dependência para JavaxValidation messages -> Adiciona nova propriedade Demais classes atualizadas para refletir validações

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/model/Categoria.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/model/Categoria.java
@@ -5,6 +5,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
 
 import lombok.Data;
 
@@ -17,6 +22,8 @@ public class Categoria {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank
+	@Size(min=3, max=20)
 	private String nome;
 	
 }
--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/resource/CategoriaResource.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/resource/CategoriaResource.java
@@ -5,16 +5,15 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -35,7 +34,8 @@ public class CategoriaResource {
 	}
 
 	@PostMapping
-	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response){
+	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response){
+
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
 		
 		URI uri = ServletUriComponentsBuilder
