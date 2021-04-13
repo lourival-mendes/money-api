c0b55402fc013f09312a4c1660a64cf00393ddb1
Lourival Mendes <minhavirtude@gmail.com>
2021-04-12 21:14:46 -0300

    Atualizações para acrescimo de delete em Categoria e Pessoa Resources.

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/resource/CategoriaResource.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/resource/CategoriaResource.java
@@ -1,6 +1,5 @@
 package com.algaworks.algamoneyapi.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,48 +7,62 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.algaworks.algamoneyapi.event.RecursoCriadoEvent;
 import com.algaworks.algamoneyapi.model.Categoria;
 import com.algaworks.algamoneyapi.repository.CategoriaRepository;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
-	
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
+	@Autowired
+	private ApplicationEventPublisher applicationEventPublisher;
+
 	@GetMapping
-	public List<Categoria> listar(){
-		return categoriaRepository.findAll(); 
+	public List<Categoria> listar() {
+		return categoriaRepository.findAll();
 	}
 
 	@PostMapping
-	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response){
+	public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
-		
-		URI uri = ServletUriComponentsBuilder
-		.fromCurrentRequestUri()
-		.path("/{codigo}")
-		.buildAndExpand(categoriaSalva.getId()).toUri();
-				
-		return ResponseEntity.created(uri).body(categoriaSalva);
+
+		applicationEventPublisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getId()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Categoria> buscarPeloId(@PathVariable Long id) {
 		Optional<Categoria> categoriaOptional = categoriaRepository.findById(id);
-		return categoriaOptional.isPresent() ? ResponseEntity.ok(categoriaOptional.get()): ResponseEntity.notFound().build();
+		return categoriaOptional.isPresent() ? ResponseEntity.ok(categoriaOptional.get())
+				: ResponseEntity.notFound().build();
 	}
 
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> removerPeloId(@PathVariable Long id) {
+
+		Optional<Categoria> categoriaOptional = categoriaRepository.findById(id);
+
+		if (categoriaOptional.isPresent()) {
+			categoriaRepository.deleteById(id);
+			return ResponseEntity.noContent().build();
+		} else
+			return ResponseEntity.notFound().build();
+
+	}
 }
--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/resource/PessoaResource.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/resource/PessoaResource.java
@@ -1,6 +1,5 @@
 package com.algaworks.algamoneyapi.resource;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,15 +7,18 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.algaworks.algamoneyapi.event.RecursoCriadoEvent;
 import com.algaworks.algamoneyapi.model.Pessoa;
 import com.algaworks.algamoneyapi.repository.PessoaRepository;
 
@@ -27,6 +29,9 @@ public class PessoaResource {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 
+	@Autowired
+	private ApplicationEventPublisher applicationEventPublisher;
+
 	@GetMapping
 	public List<Pessoa> listar() {
 		return pessoaRepository.findAll();
@@ -36,17 +41,28 @@ public class PessoaResource {
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+		applicationEventPublisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getId()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-				.buildAndExpand(pessoaSalva.getId()).toUri();
-
-		return ResponseEntity.created(uri).body(pessoaSalva);
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Pessoa> buscarPeloId(@PathVariable Long id) {
 		Optional<Pessoa> pessoaOptional = pessoaRepository.findById(id);
 		return pessoaOptional.isPresent() ? ResponseEntity.ok(pessoaOptional.get()) : ResponseEntity.notFound().build();
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> removerPeloId(@PathVariable Long id) {
+
+		Optional<Pessoa> pessoaOptional = pessoaRepository.findById(id);
+
+		if (pessoaOptional.isPresent()) {
+			pessoaRepository.deleteById(id);
+			return ResponseEntity.noContent().build();
+		} else
+			return ResponseEntity.notFound().build();
+
 	}
 
 }
