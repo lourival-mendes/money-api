fc2725ee00f8788ef2e52c5ce11768a776e1de5d
Lourival Mendes <minhavirtude@gmail.com>
2021-04-14 16:05:05 -0300

    Criação do método filtrar.

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/resource/LancamentoResource.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/resource/LancamentoResource.java
@@ -32,6 +32,7 @@ import com.algaworks.algamoneyapi.event.RecursoCriadoEvent;
 import com.algaworks.algamoneyapi.exceptionhandler.AlgaMoneyResponseEntityExceptionHandler.Erro;
 import com.algaworks.algamoneyapi.model.Lancamento;
 import com.algaworks.algamoneyapi.repository.LancamentoRepository;
+import com.algaworks.algamoneyapi.resource.repository.filter.LancamentoFilter;
 import com.algaworks.algamoneyapi.service.LancamentoService;
 import com.algaworks.algamoneyapi.service.exception.PessoaInexistenteOuInativaException;
 
@@ -53,6 +54,12 @@ public class LancamentoResource {
 
 	@GetMapping
 	public List<Lancamento> listar() {
+		return lancamentoRepository.findAll();
+	}
+
+	@GetMapping("filtros")
+	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter) {
+//		return lancamentoRepository.filter(lancamentoFilter);
 		return lancamentoRepository.findAll();
 	}
 
