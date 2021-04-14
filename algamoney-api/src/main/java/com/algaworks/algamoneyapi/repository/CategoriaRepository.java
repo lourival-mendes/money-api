6e5681432548c7be593439046a397002331cc9eb
Lourival Mendes <minhavirtude@gmail.com>
2021-04-14 16:06:17 -0300

    Apenas organizaão automática para formatação.

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/repository/CategoriaRepository.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/repository/CategoriaRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algaworks.algamoneyapi.model.Categoria;
 
-public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
 }
