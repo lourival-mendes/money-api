b26bffdb1cdc76ce50fd93b56315c25b2a27ba35
Lourival Mendes <minhavirtude@gmail.com>
2021-04-14 00:11:56 -0300

    Alteração para criação de pessoa passar por LançamentoService e criação de método de exceção.

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/resource/LancamentoResource.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/resource/LancamentoResource.java
@@ -1,7 +1,9 @@
 package com.algaworks.algamoneyapi.resource;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
@@ -9,10 +11,13 @@ import javax.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,8 +29,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algamoneyapi.event.RecursoAtualizadoEvent;
 import com.algaworks.algamoneyapi.event.RecursoCriadoEvent;
+import com.algaworks.algamoneyapi.exceptionhandler.AlgaMoneyResponseEntityExceptionHandler.Erro;
 import com.algaworks.algamoneyapi.model.Lancamento;
 import com.algaworks.algamoneyapi.repository.LancamentoRepository;
+import com.algaworks.algamoneyapi.service.LancamentoService;
+import com.algaworks.algamoneyapi.service.exception.PessoaInexistenteOuInativaException;
 
 @RestController
 @RequestMapping("/lancamentos")
@@ -33,6 +41,12 @@ public class LancamentoResource {
 
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
+
+	@Autowired
+	private MessageSource messageSource;
+
+	@Autowired
+	private LancamentoService lancamentoService;
 
 	@Autowired
 	private ApplicationEventPublisher applicationEventPublisher;
@@ -45,7 +59,7 @@ public class LancamentoResource {
 	@PostMapping
 	public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
 
-		Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+		Lancamento lancamentoSalvo = lancamentoService.save(lancamento);
 
 		applicationEventPublisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getId()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
@@ -60,7 +74,7 @@ public class LancamentoResource {
 
 		BeanUtils.copyProperties(lancamento, lancamentoSalvo, "id");
 
-		Lancamento lancamentoAtualizado = lancamentoRepository.save(lancamentoSalvo);
+		Lancamento lancamentoAtualizado = lancamentoService.save(lancamentoSalvo);
 		applicationEventPublisher.publishEvent(new RecursoAtualizadoEvent(this, response));
 		return ResponseEntity.status(HttpStatus.OK).body(lancamentoAtualizado);
 
@@ -93,4 +107,16 @@ public class LancamentoResource {
 	public void removerPeloId(@PathVariable Long id) {
 		lancamentoRepository.deleteById(id);
 	}
+
+	@ExceptionHandler({ PessoaInexistenteOuInativaException.class })
+	public ResponseEntity<Object> PessoaInexistenteOuInativaException(PessoaInexistenteOuInativaException ex) {
+
+		String mensagemUsuario = messageSource.getMessage("recurso.pessoa-inexistente-ou-inativa", null,
+				LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = Optional.ofNullable(ex.getCause()).orElse(ex).toString();
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		return ResponseEntity.badRequest().body(erros);
+
+	}
+
 }
