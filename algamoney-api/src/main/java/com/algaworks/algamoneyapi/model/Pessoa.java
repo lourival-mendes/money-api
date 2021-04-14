2165119acd4fbf4841c1804d33c4d88023ad0d9b
Lourival Mendes <minhavirtude@gmail.com>
2021-04-14 00:10:41 -0300

    Acrescentado método para verificação se estado da pessoa é inativo.

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/model/Pessoa.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/model/Pessoa.java
@@ -6,9 +6,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
@@ -30,5 +33,11 @@ public class Pessoa {
 
 	@Embedded
 	private Endereco endereco;
+
+	@JsonIgnore
+	@Transient
+	public boolean isInativo() {
+		return !this.ativo;
+	}
 
 }
