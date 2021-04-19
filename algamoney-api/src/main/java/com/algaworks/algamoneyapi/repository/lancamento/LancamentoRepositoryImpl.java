f565f19b5d7462d1ae00510c754919a4fb6cfbb9
Lourival Mendes <minhavirtude@gmail.com>
2021-04-19 16:07:05 -0300

    Implementação do recurso "resumo" para lançamentos.

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/repository/lancamento/LancamentoRepositoryImpl.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/repository/lancamento/LancamentoRepositoryImpl.java
@@ -15,9 +15,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
+import com.algaworks.algamoneyapi.model.Categoria_;
 import com.algaworks.algamoneyapi.model.Lancamento;
 import com.algaworks.algamoneyapi.model.Lancamento_;
+import com.algaworks.algamoneyapi.model.Pessoa_;
 import com.algaworks.algamoneyapi.repository.filter.LancamentoFilter;
+import com.algaworks.algamoneyapi.repository.projection.LancamentoResumo;
 
 public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 
@@ -53,7 +56,7 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 		return manager.createQuery(criteria).getSingleResult();
 	}
 
-	private void adicionarRestricoesDePaginacao(TypedQuery<Lancamento> query, Pageable pageable) {
+	private void adicionarRestricoesDePaginacao(TypedQuery<?> query, Pageable pageable) {
 
 		int paginaAtual = pageable.getPageNumber();
 		int totalRegistrosPorPagina = pageable.getPageSize();
@@ -82,6 +85,28 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
 					lancamentoFilter.getDataVencimentoAte()));
 
 		return predicates.toArray(new Predicate[predicates.size()]);
+	}
+
+	@Override
+	public Page<LancamentoResumo> resumir(LancamentoFilter lancamentoFilter, Pageable pageable) {
+
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<LancamentoResumo> criteria = builder.createQuery(LancamentoResumo.class);
+		Root<Lancamento> root = criteria.from(Lancamento.class);
+
+		criteria.select(builder.construct(LancamentoResumo.class, root.get(Lancamento_.id),
+				root.get(Lancamento_.descricao), root.get(Lancamento_.dataVencimento),
+				root.get(Lancamento_.dataPagamento), root.get(Lancamento_.valor), root.get(Lancamento_.tipo),
+				root.get(Lancamento_.categoria).get(Categoria_.nome), root.get(Lancamento_.pessoa).get(Pessoa_.nome)));
+
+		Predicate[] predicates = criarRestricoes(lancamentoFilter, builder, root);
+		criteria.where(predicates);
+
+		TypedQuery<LancamentoResumo> query = manager.createQuery(criteria);
+
+		adicionarRestricoesDePaginacao(query, pageable);
+
+		return new PageImpl<>(query.getResultList(), pageable, total(lancamentoFilter));
 	}
 
 }
--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/resource/LancamentoResource.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/resource/LancamentoResource.java
@@ -36,6 +36,7 @@ import com.algaworks.algamoneyapi.exceptionhandler.AlgaMoneyResponseEntityExcept
 import com.algaworks.algamoneyapi.model.Lancamento;
 import com.algaworks.algamoneyapi.repository.LancamentoRepository;
 import com.algaworks.algamoneyapi.repository.filter.LancamentoFilter;
+import com.algaworks.algamoneyapi.repository.projection.LancamentoResumo;
 import com.algaworks.algamoneyapi.service.LancamentoService;
 import com.algaworks.algamoneyapi.service.exception.PessoaInexistenteOuInativaException;
 
@@ -59,6 +60,12 @@ public class LancamentoResource {
 	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
 	public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
 		return lancamentoRepository.filtrar(lancamentoFilter, pageable);
+	}
+
+	@GetMapping(params = "resumo")
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
+	public Page<LancamentoResumo> resumir(LancamentoFilter lancamentoFilter, Pageable pageable) {
+		return lancamentoRepository.resumir(lancamentoFilter, pageable);
 	}
 
 	@PostMapping
