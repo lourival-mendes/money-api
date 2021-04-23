981578b9472e2acd4f22bbcd3524b5315e80199b
Lourival Mendes <minhavirtude@gmail.com>
2021-04-23 11:03:29 -0300

    Alteração do metodo main para novo "criarSenha"

--- algamoney-api/src/main/java/com/algaworks/algamoney/api/util/GeradorDeSenha.java
+++ algamoney-api/src/main/java/com/algaworks/algamoney/api/util/GeradorDeSenha.java
@@ -4,7 +4,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class GeradorDeSenha {
 
-	public static void main(String[] args) {
+	public void gerarSenha(String[] args) {
 		
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		System.out.println(encoder.encode("admin"));
