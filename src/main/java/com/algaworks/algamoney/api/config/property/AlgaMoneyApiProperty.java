e40d05be5d034fb0749455b925d733a5363303e2
lourival-mendes <minhavirtude@gmail.com>
2021-05-26 19:40:13 -0300

    Atribuição fixa para porta algamoney.mail.port=587

--- src/main/java/com/algaworks/algamoney/api/config/property/AlgaMoneyApiProperty.java
+++ src/main/java/com/algaworks/algamoney/api/config/property/AlgaMoneyApiProperty.java
@@ -29,7 +29,7 @@ public class AlgaMoneyApiProperty {
 	public static class Mail {
 
 		private String host;
-		private String port;
+		private int port;
 		private String username;
 		private String password;
 		private String email;
