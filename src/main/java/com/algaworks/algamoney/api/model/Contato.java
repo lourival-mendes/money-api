259dcb58419bced218f3bb729be8cd669569e4e7
lourival-mendes <minhavirtude@gmail.com>
2021-05-27 13:54:40 -0300

    Criação de contatos para pessoa.

---
+++ src/main/java/com/algaworks/algamoney/api/model/Contato.java
@@ -0,0 +1,42 @@
+package com.algaworks.algamoney.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "contato")
+public class Contato {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank
+	@Size(min = 3, max = 20)
+	private String nome;
+
+	@NotBlank
+	@Size(min = 5, max = 100)
+	private String email;
+
+	@NotBlank
+	@Size(min = 10, max = 20)
+	private String telefone;
+
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "id_pessoa")
+	private Pessoa pessoa;
+
+}
--- src/main/java/com/algaworks/algamoney/api/model/Pessoa.java
+++ src/main/java/com/algaworks/algamoney/api/model/Pessoa.java
@@ -1,12 +1,17 @@
 package com.algaworks.algamoney.api.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -39,5 +44,9 @@ public class Pessoa {
 	public boolean isInativo() {
 		return !this.ativo;
 	}
+
+	@Valid
+	@OneToMany(mappedBy = "pessoa", cascade = CascadeType.ALL)
+	private List<Contato> contatos;
 
 }
