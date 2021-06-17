e5243f5232b5efa1dd1151e3aec1b94022664112
Lourival Mendes <minhavirtude@gmail.com>
2021-06-17 13:59:48 -0300

    Adição de fetch = FetchType.EAGER para atributo contatos

--- src/main/java/br/com/lourivalmendes/money/api/model/Pessoa.java
+++ src/main/java/br/com/lourivalmendes/money/api/model/Pessoa.java
@@ -5,13 +5,13 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -47,8 +47,7 @@ public class Pessoa {
 	}
 
 	@JsonIgnoreProperties("pessoa")
-	@Valid
-	@OneToMany(mappedBy = "pessoa", cascade = CascadeType.ALL, orphanRemoval = true)
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "pessoa", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Contato> contatos;
 
 }
